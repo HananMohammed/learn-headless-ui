db17cb947030b43911b2832d57e63346c447d763
hananmohamed111995 <149586843+hananmohamed111995@users.noreply.github.com>
2024-04-28 20:30:02 +0300

    Dialog

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,99 @@
 <template>
   <div class="min-h-screen bg-gray-200 text-gray-900">
     <div class="container mx-auto">
-      <h5 class="font-bold text-xl pt-2"> Menu (Dropdown)</h5>
+      <h5 class="font-bold text-xl pt-2"> Dialog (Modal)</h5>
 
-      <Menu>
-        <MenuButton class="flex items-center bg-blue-400 text-white rounded px-4 py-2">
-          <span>More</span>
-          <ChevronDownIcon class="w-5 h-5 ml-2"/>
-        </MenuButton>
+      <button class="flex items-center bg-blue-400 text-white rounded px-4 py-2"
+              @click="openModal">
+          open Modal
+      </button>
 
-        <transition enter-active-class="transition duration-200 ease-out"
-                    enter-from-class="transform scale-75 opacity-0"
-                    enter-to-class="transform scale-100 opacity-100"
-                    leave-active-class="transition duration-100 ease-in"
-                    leave-from-class="transform scale-100 opacity-100"
-                    leave-to-class="transform scale-75 opacity-0">
+      <TransitionRoot appear :show="isOpen" as="template">
+        <Dialog as="div" @close="closeModal" class="relative z-10">
+          <TransitionChild
+              as="template"
+              enter="duration-300 ease-out"
+              enter-from="opacity-0"
+              enter-to="opacity-100"
+              leave="duration-200 ease-in"
+              leave-from="opacity-100"
+              leave-to="opacity-0"
+          >
+            <div class="fixed inset-0 bg-black/25" />
+          </TransitionChild>
 
-          <MenuItems class="absolute bg-white rounded-lg shadow-md flex flex-col focus:outline-none mt-2 w-56 origin-top-left">
-            <div class="px-1 py-1">
-              <MenuItem v-slot="{ active }">
-                <button :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]">
-                  <PencilSquareIcon class="mr-2 h-5 w-5 text-violet-400"/>
-                  <span class="ml-2">Edit</span>
-                </button>
-              </MenuItem>
-
-              <MenuItem v-slot="{ active }">
-                <button :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]">
-                  <TrashIcon class="mr-2 h-5 w-5 text-violet-400"/>
-                  <span class="ml-2">Delete</span>
-                </button>
-              </MenuItem>
-            </div>
-
-            <div class="px-1 py-1">
-              <MenuItem v-slot="{ active }">
-                <button
-                    :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]"
+          <div class="fixed inset-0 overflow-y-auto">
+            <div
+                class="flex min-h-full items-center justify-center p-4 text-center"
+            >
+              <TransitionChild
+                  as="template"
+                  enter="duration-300 ease-out"
+                  enter-from="opacity-0 scale-95 rotate-180"
+                  enter-to="opacity-100 scale-100 rotate-0"
+                  leave="duration-200 ease-in"
+                  leave-from="opacity-100 scale-100 rotate-0"
+                  leave-to="opacity-0 scale-95 rotate-180"
+              >
+                <DialogPanel
+                    class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
                 >
-                  <ArchiveBoxIcon
-                      :active="active"
-                      class="mr-2 h-5 w-5 text-violet-400"
-                      aria-hidden="true"
-                  />
-                  Archive
-                </button>
-              </MenuItem>
-              <MenuItem v-slot="{ active }">
-                <button
-                    :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]"
-                >
-                  <TicketIcon
-                      :active="active"
-                      class="mr-2 h-5 w-5 text-violet-400"
-                      aria-hidden="true"
-                  />
-                  Move
-                </button>
-              </MenuItem>
+                  <DialogTitle
+                      as="h3"
+                      class="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Payment successful
+                    <button class="absolute top-3 right-3"   @click="closeModal">
+                      <XCircleIcon class="h-5 w-5 text-gray-500" />
+                    </button>
+                  </DialogTitle>
+                  <div class="mt-2">
+                    <p class="text-sm text-gray-500">
+                      Your payment has been successfully submitted. We’ve sent you
+                      an email with all of the details of your order.
+                    </p>
+                  </div>
+
+                  <div class="mt-4">
+                    <button
+                        type="button"
+                        class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        @click="closeModal"
+                    >
+                      Got it, thanks!
+                    </button>
+                  </div>
+                </DialogPanel>
+              </TransitionChild>
             </div>
-
-          </MenuItems>
-        </transition>
-      </Menu>
-
+          </div>
+        </Dialog>
+      </TransitionRoot>
 
     </div>
   </div>
 </template>
 
 <script setup>
-import {Menu, MenuButton, MenuItems, MenuItem} from '@headlessui/vue'
-import {ChevronDownIcon, PencilSquareIcon, TrashIcon, ArchiveBoxIcon, TicketIcon} from '@heroicons/vue/24/solid'
+import { ref } from 'vue'
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
 
+import {XCircleIcon} from '@heroicons/vue/24/outline'
+
+const isOpen = ref(false)
+
+function closeModal() {
+  isOpen.value = false
+}
+function openModal() {
+  isOpen.value = true
+}
 </script>
 
 <style scoped>
